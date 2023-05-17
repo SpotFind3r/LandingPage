e16f0e4511e2d32bdca3c4a7fb9d59fee331f1e7
SebastiaNCastro22 <89089487+SebastiaNCastro22@users.noreply.github.com>
2023-05-16 21:36:11 -0500

    Delete main1.js

--- main1.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-menu").classList.toggle("show");
-  });
-  
-  ScrollReveal().reveal('.showcase');
-  ScrollReveal().reveal('.service', { delay: 500 });
-  ScrollReveal().reveal('.about-us', { delay: 500 });
-  ScrollReveal().reveal('.contact-us', { delay: 500 });
